94a32feed5fe25d7f4e46a77ed714b0dbe6da723
GTA <gregory.taudin@exfrabrica.io>
2022-03-23 16:00:48 +0100

    Fix async service function.

--- part-2/api/src/plugins/awesome-help/server/services/help.js
+++ part-2/api/src/plugins/awesome-help/server/services/help.js
@@ -5,7 +5,7 @@ module.exports = ({ strapi }) => {
   const findMany = async (params, populate) => {
     let helpEntities = undefined;
     try {
-      helpEntities = query.findMany(params, populate);
+      helpEntities = await query.findMany(params, populate);
     }
     catch (exp) {
       throw new Error(`Help Service: An error occured when get help: ${exp.message}`);
