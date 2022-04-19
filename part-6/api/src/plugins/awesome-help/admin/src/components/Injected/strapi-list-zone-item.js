3555228f1a620f971dc83485cff64f3742b4b8ac
GTA <gregory.taudin@exfrabrica.io>
2022-04-19 10:47:37 +0200

    Fix Awesome Help plugin with Strapi team review.  - Replace button switch with ToogleInput

--- part-6/api/src/plugins/awesome-help/admin/src/components/Injected/strapi-list-zone-item.js
+++ part-6/api/src/plugins/awesome-help/admin/src/components/Injected/strapi-list-zone-item.js
@@ -4,12 +4,10 @@ import { useIntl } from 'react-intl';
 import getTrad from '../../utils/getTrad';
 // Strapi Design System
 import { Portal } from '@strapi/design-system/Portal';
-import { Button } from '@strapi/design-system/Button';
 import { Typography } from '@strapi/design-system/Typography';
 import { Divider } from '@strapi/design-system/Divider';
 import { Box } from '@strapi/design-system/Box';
-import EyeStriked from '@strapi/icons/EyeStriked';
-import Eye from '@strapi/icons/Eye';
+import { ToggleInput } from '@strapi/design-system/ToggleInput';
 // Context from Strapi Helper.
 import { useCMEditViewDataManager } from '@strapi/helper-plugin';
 //API proxy
@@ -18,7 +16,7 @@ import SettingsProxy from "../../api/settings-proxy";
 // Strapi UI Decorator component
 import { StrapiUIDecorator } from './strapi-ui-decorator'
 
-export const StrapiListZoneItem = ({strapi}) => {
+export const StrapiListZoneItem = ({ strapi }) => {
   const { formatMessage } = useIntl();
   const { slug } = useCMEditViewDataManager();
   const [results, setResults] = useState([]);
@@ -34,11 +32,6 @@ export const StrapiListZoneItem = ({strapi}) => {
       }
     });
   }, []);
-
-  const handleClick = (e) => {
-    e.preventDefault();
-    setEnable(!enable);
-  }
 
   return (
     <Box
@@ -61,23 +54,16 @@ export const StrapiListZoneItem = ({strapi}) => {
         {`${formatMessage({ id: getTrad("plugin.help.zone.available") })} ${results.length}`}
       </Typography>
 
-      {enable ?
-        <>
-          <Box paddingTop={2} paddingBottom={6}>
-          </Box>
-          <Button fullWidth onClick={handleClick} size="S" startIcon={<EyeStriked />} variant="danger-light">
-            {formatMessage({ id: getTrad("plugin.help.zone.button.disable") })}
-          </Button>
-        </> :
-        <>
-          {results && results.length > 0 ? <>
-            <Box paddingTop={2} paddingBottom={6}>
-            </Box>
-            <Button fullWidth onClick={handleClick} size="S" startIcon={<Eye />} variant="secondary">
-              {formatMessage({ id: getTrad("plugin.help.zone.button.enable") })}
-            </Button></>
-            : <></>}
-        </>}
+      <Box paddingBottom={4}></Box>
+
+      <ToggleInput
+        checked={enable}
+        label={formatMessage({ id: getTrad("plugin.help.zone.button.enable") })}
+        offLabel="Off"
+        onLabel="On"
+        onChange={(e) => setEnable(!enable)}
+      />
+
       <Portal>
         <StrapiUIDecorator helpItems={results} enable={enable} />
       </Portal>
